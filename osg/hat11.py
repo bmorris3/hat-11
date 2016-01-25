45a75eee87888842d068fd33c7767e8c806ddde8
Brett Morris <brettmorris21@gmail.com>
2016-01-25 15:11:11 -0800

    Prepping for open science grid

---
+++ osg/hat11.py
@@ -0,0 +1,65 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import os
+from utils import (STSPRun, get_transit_parameters)
+from glob import glob
+
+run_name = 'hat11'
+
+top_level_output_dir = os.path.join('/astro/store/scratch/tmp/bmmorris/stsp_20160124',
+                                    run_name)
+#light_curve_path = 'kepler17_whole.dat'
+#parameter_file_path = 'kep17.params'
+#p_orb, t_0, tdur, p_rot, Rp_Rs, impact, incl_orb, Teff, sden = get_transit_parameters(parameter_file_path)
+
+planet_properties = dict(n_planets=1,
+                         first_mid_transit_time=2454605.89154,
+                         period=4.88780236,
+                         transit_depth=0.00343**0.5,
+                         transit_duration_days=0.0982,
+                         impact_parameter=0.121,
+                         inclination=89.45042,
+                         orbit_lambda=106,
+                         ecosw=0.261,
+                         esinw=0.085
+                         )
+
+stellar_properties = dict(mean_stellar_density=1.81004,
+                          stellar_rotation_period=29.984,
+                          stellar_temperature=4780,
+                          stellar_metallicity=0,
+                          tilt_stellar_rotation_axis=80,
+                          four_param_limb_darkening=' '.join(map(str, [0, 0.86730, 0, -0.15162])),
+                          n_ld_rings=40
+                          )
+
+spot_properties = dict(lightcurve_path=None,
+                       flattened_flag=1,
+                       n_spots=4,
+                       fractional_spot_contrast=0.7
+                       )
+
+# For an unseeded run:
+action_properties = dict(random_seed=74384338,
+                         a_scale=2.5,
+                         n_chains=20,
+                         n_steps=500,
+                         calc_brightness=1
+                         )
+
+run = STSPRun(parameter_file_path=None,
+              light_curve_path=None,
+              executable_path='/astro/users/bmmorris/git/hat-11/osg/{0}.csh'.format(run_name),
+              output_dir_path=top_level_output_dir,
+              initial_dir=top_level_output_dir,
+              condor_config_path=os.path.join('./', run_name+'.condor'),
+              planet_properties=planet_properties,
+              stellar_properties=stellar_properties,
+              spot_properties=spot_properties,
+              action_properties=action_properties,
+              n_restarts=2)
+
+run.copy_data_files(transit_paths=glob('/astro/users/bmmorris/git/hat-11/lightcurves/hat11_single_transits/*.txt'))
+run.create_runs()
+run.make_condor_config()
---
+++ osg/progress.py
@@ -0,0 +1,93 @@
+import os
+from glob import glob
+import shutil
+import numpy as np
+
+run_name = 'hat11'
+
+top_level_output_dir = os.path.join('/astro/store/scratch/tmp/bmmorris/stsp_20160124/',
+                                    run_name)
+                                    
+#finished_runs = glob(os.path.join(top_level_output_dir, 
+#                                  "window???/run???/*parambest.txt"))
+                                  
+def find_windows_to_continue(output_dir_path):
+    """
+    Parameters
+    ----------
+    output_dir_path : str
+        Path to outputs
+
+    Returns
+    -------
+    runs_ready_to_begin : list
+        Runs that are ready to be begun
+    """
+    runs_ready_to_begin = []
+    completed_runs = []
+    runs_in_progress = []
+    window_dirs = sorted(glob(os.path.join(output_dir_path, 'window*')))
+
+    for window_dir in window_dirs:
+        window_index = int(window_dir.split('window')[1])
+        run_dirs = sorted(glob(os.path.join(window_dir, 'run*')))
+        this_window_is_running_or_assigned = False
+
+        for run_dir in run_dirs:
+            run_index = int(run_dir.split('run')[1])
+
+            run_id = (window_index, run_index)
+            current_finalparam_path = os.path.join(run_dir, "window{0:03d}_run{1:03d}_finalparam.txt".format(window_index, run_index))
+            current_initalized_path = os.path.join(run_dir, "initialized.txt")
+
+            # If the current directory has its own finalparam.txt file, the run
+            # has been completed
+            if os.path.exists(current_finalparam_path):
+                completed_runs.append(run_id)
+
+            # If the current directory doesn't have its own finalparam.txt file,
+            # but it has an initialized.txt file, the run is in progress
+            elif os.path.exists(current_initalized_path):
+                runs_in_progress.append(run_id)
+                this_window_is_running_or_assigned = True
+
+            # If run is not complete or in progress, it is uninitialized:
+            elif not this_window_is_running_or_assigned:
+                runs_ready_to_begin.append(run_id)
+                this_window_is_running_or_assigned = True
+    return completed_runs, runs_in_progress
+
+if __name__ == '__main__':
+    completed_runs, runs_in_progress = find_windows_to_continue(top_level_output_dir)
+
+    print("Runs in progress ({0}): {1}".format(len(runs_in_progress), runs_in_progress))
+#    print("Total runs in progress:", len(runs_in_progress))
+
+    import matplotlib.pyplot as plt
+
+    last_completed_runs = dict()
+    for window, run in completed_runs:
+        if window not in last_completed_runs or last_completed_runs[window] < run:
+            last_completed_runs[window] = run
+
+    in_progress_runs = [run for window, run in runs_in_progress]
+
+    window_number, run_number = zip(*last_completed_runs.items())
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+    ax[0].plot(window_number, run_number, lw=2)#ls='steps', lw=2)
+    ax[0].set_xlabel('Window')
+    ax[0].set_ylabel('Run')
+    ax[0].axhline(np.mean(run_number))
+
+    ax[1].hist(run_number, max(run_number), range=[0, max(run_number)],
+               label='Last Completed', color='k', alpha=0.5)
+
+    if len(in_progress_runs) > 0:
+        ax[1].hist(in_progress_runs, max(in_progress_runs),
+                   range=[0, max(in_progress_runs)],
+                   label='In Progress', color='r', alpha=0.5)
+    ax[1].set_xlabel('Latest completed runs')
+    ax[1].legend(loc='upper center')
+    plt.show()
+
